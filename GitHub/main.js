b23ba7e1741c4e453c0c0a37d8e8d64b91ecb91b
Dima305286 <Dobychinivan@mail.ru>
2026-03-29 13:24:29 +0300

    m5

---
+++ GitHub/main.js
@@ -0,0 +1,20 @@
+let searchValue = document.getElementById("searchValue");
+let searchBtn = document.getElementById("searchBtn");
+let card = document.getElementById("card");
+let load = document.getElementById("load");
+
+let dataCard;
+
+async function getCard() {
+    card.classList.add("none");
+    load.classList.remove("none");
+
+    const response = await fetch("https://api.github.com/users/KrasnovDaniill");
+    if (response.ok) {
+        dataCard = await response.json()
+        console.log(dataCard)
+    }
+
+}
+
+searchBtn.addEventListener("click", getCard)

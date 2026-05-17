0ef3be0d4d64a2cf3b343087a98e05fae0687b92
Dima305286 <Dobychinivan@mail.ru>
2026-05-17 13:27:48 +0300

    film

---
+++ films/src/pages/Seasons.vue
@@ -0,0 +1,30 @@
+<script setup>
+import CardSeasons from "../components/CardSeasons.vue";
+import { onMounted, ref } from "vue";
+
+let cards = ref([
+  {
+    img: "",
+    title: "заголовок",
+    desc: "описание",
+  },
+  {
+    img: "",
+    title: "заголовок",
+    desc: "описание",
+  },
+]);
+</script>
+
+<template>
+  <section>
+    <CardSeasons v-for="(card, i) in cards" :key="i" :card="card" />
+  </section>
+</template>
+
+<style scoped>
+section {
+  display: grid;
+  gap: 40px;
+}
+</style>
